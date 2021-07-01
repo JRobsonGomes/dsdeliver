e43d7a692f8bf957ed6bef70ff7ea6e04cb13387
JRobsonGomes <jose.gomes@flashcourier.com.br>
2021-07-01 18:05:13 -0300

    Listing products

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from 'react';
+import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 import './styles.css';
 
 const Orders = () => {
+    const [products, setProducts] = useState<Product[]>([]);
+    
+    useEffect(() => {
+        fetchProducts().then(
+            response => setProducts(response.data)
+        ).catch(
+            error => console.log(error)
+        )
+    }, [])
+
     return (
-       <div className="orders-container">
-           <StepsHeader/>
-       </div>
+        <div className="orders-container">
+            <StepsHeader />
+            <ProductsList products={products} />
+        </div>
     )
 }
 
